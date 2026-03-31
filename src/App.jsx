85c4d1468d51209fc8fd0fda318368d2c02868af
Aitunuk <karypovaaitunuk3@gmail.com>
2026-03-31 17:48:40 +0300

    Firts commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home'
+import Catalog from './components/Catalog'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <Catalog/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Catalog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../styles/Catalog.module.css"
+
+const Catalog = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Catalog
+
