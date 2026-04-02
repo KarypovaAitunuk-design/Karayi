1a5edbe3ef304d1fd0c62db3b083fff711608dc2
azizkirgiz11 <azizkirgiz11@gmail.com>
2026-04-02 16:37:45 +0600

    create catalog popular items

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import styles from "../styles/Catalog.module.css"
+import { food } from '../data'
 
 const Catalog = () => {
   return (
     <div>
-
+      <div className={styles.con}>
+        <p>Popular Items</p>
+        {/* <div><button></button> <button></button></div> */}
+          {food.map((item) => (
+            <div className={styles.food} key={item.id}>
+              <img className={styles.img} src={item.image} alt="" />
+              <p className={styles.name}>{item.name}</p>
+              <p className={styles.place}>{item.place}</p>
+              <p className={styles.price}>${item.price}</p>
+              <button>Order Now</button>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
 
 export default Catalog
-
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const food = [
+{
+id: 1,
+name: "Cheese Burger",
+place: "Burger Arena",
+price: 3.88,
+image:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTluTqJR8vJHSek2-eubng3RnM98fxUMBvbTPUu3ZuzRCCWHSod"},
+{
+id: 2,
+name: "Toffe`s Cake",
+place: "Top sticks",
+price: 4.11,
+image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq7nLkH9hkT8A1xFF4yQGqILYjSMC-BW1sGT3MLEDrMOKrTIC3"},
+
+{
+id: 3,
+name: "Dancake",
+place: "Cake World",
+price: 1.99,
+image:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTfz_aMW7nbQ6TY-IdxdWOB5Mj70A6CW8sCyLMNaySOzHiV7k4R"},
+
+{
+id: 4,
+name: "Crispy Sandwitch",
+place: "Fastfood Dine",
+price: 3.49,
+image:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTl85B3HDgoGSjmi56-GW0Cbb1YiscD4_kGnGO5LJ_AgvX3mwDg"},
+
+{
+id: 5,
+name: "Thai Soup",
+place: "Foody Man",
+price: 2.79,
+image:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSSXN3JZQ8framHJfCzP7WtZ53MhRP9TJGNaizoHoHHkJpDhR-Z"},
+
+];
